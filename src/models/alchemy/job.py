e3473092d7d25f50ba13448254efe4622f1f3d44
emaeblini <erikislamov06@yandex.ru>
2026-06-26 17:17:47 +0500

    fix убрал лишние пустые строки

--- src/models/alchemy/job.py
+++ src/models/alchemy/job.py
@@ -8,23 +8,13 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 class Job(Base):
     __tablename__ = 'jobs'
-
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), nullable=False)
-
     title: Mapped[str] = mapped_column(String, nullable=False)
-
     description: Mapped[str] = mapped_column(String, nullable=False)
-
     salary_from: Mapped[Decimal] = mapped_column(Numeric(10, 2))
-
     salary_to: Mapped[Decimal] = mapped_column(Numeric(10, 2))
-
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
-
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), default=lambda: datetime.now(timezone.utc))
-
     user = relationship('User', back_populates='jobs')
-
     responses = relationship('Response', back_populates='job', cascade='all, delete-orphan')
--- src/models/alchemy/user.py
+++ src/models/alchemy/user.py
@@ -8,17 +8,10 @@ class User(Base):
     __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-
     email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
-
     name: Mapped[str] = mapped_column(String, nullable=False)
-
     hashed_password: Mapped[str] = mapped_column(String, nullable=False)
-
     is_company:Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
-
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), default=lambda: datetime.now(timezone.utc))
-
     jobs = relationship('Job', back_populates='user', cascade='all, delete-orphan')
-
     responses = relationship('Response', back_populates='user', cascade='all, delete-orphan')
