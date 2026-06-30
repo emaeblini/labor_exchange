c43a44bb49f8e7478de5daa1ae3115486de51153
emaeblini <erikislamov06@yandex.ru>
2026-06-30 18:17:43 +0500

    fix: переписал сервисы через новый uow

--- src/services/job_service.py
+++ src/services/job_service.py
@@ -1,56 +1,47 @@
-from models.dto.job import JobCreateDTO, JobDTO
-from models.dto.response import ResponseCreateDTO, ResponseDTO
-from services.exceptions import ObjectDoesntExistsException, SystemLogicError
+from bases.services.base_service import BaseService
 from bases.uows.job_uow import JobUOW
+from models.dto.job import JobCreateDTO, JobDTO
+from services.exceptions import ObjectDoesntExistsException
 
 
-class JobService:
+class JobService(BaseService):
     def __init__(self, uow: JobUOW) -> None:
         self.uow = uow
 
-    async def create_job(self, job_schema: JobCreateDTO) -> JobDTO:
+    async def create_job(self, job_create_data: JobCreateDTO) -> JobDTO:
+        """
+        Создать вакансию.
+        """
+
         async with self.uow as uow:
-            job = await uow.job_repository.create_job(job_schema)
+            job = await uow.repository.create(job_create_data)
             await uow.commit()
 
             return job
 
     async def get_all_jobs(self, limit: int = 100, skip: int = 0) -> list[JobDTO]:
+        """
+        Получить список всех активных вакансий.
+        """
+
         async with self.uow as uow:
-            return await uow.job_repository.get_all_jobs(
+            jobs = await uow.repository.list(
                 limit=limit,
                 skip=skip,
+                is_active=True,
             )
 
+            return list(jobs)
+
     async def get_job_by_id(self, job_id: int) -> JobDTO:
+        """
+        Получить вакансию по идентификатору.
+        """
+
         async with self.uow as uow:
-            job = await uow.job_repository.get_job_by_id(job_id)
+            job = await uow.repository.retrieve(id=job_id)
 
             if job is None:
                 raise ObjectDoesntExistsException("Вакансия не найдена")
 
             return job
-
-    async def response_job(self, response_schema: ResponseCreateDTO) -> ResponseDTO:
-        async with self.uow as uow:
-            job = await uow.job_repository.get_job_by_id(response_schema.job_id)
-
-            if job is None:
-                raise ObjectDoesntExistsException("Вакансия не найдена")
-
-            if not job.is_active:
-                raise SystemLogicError("Нельзя откликнуться на неактивную вакансию")
-
-            response = await uow.response_repository.response_job(response_schema)
-            await uow.commit()
-
-            return response
-
-    async def get_responses_by_job_id(self, job_id: int) -> list[ResponseDTO]:
-        async with self.uow as uow:
-            job = await uow.job_repository.get_job_by_id(job_id)
-
-            if job is None:
-                raise ObjectDoesntExistsException("Вакансия не найдена")
-
-            return await uow.response_repository.get_responses_by_job_id(job_id)
